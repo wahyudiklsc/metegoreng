698bda252b42d65f521ecb1fa09d0814e2c93465
wahyudiklsc <wahyudiklsc@gmail.com>
2024-12-09 05:56:01 +0000

    first commit

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@
 
     // Countdown Timer
     function countDownTimer() {	
-        var endTime = new Date("31 December 2023 10:00:00 GMT+00:00");
+        var endTime = new Date("9 Januari 2025 10:00:00 GMT+00:00");
         endTime = (Date.parse(endTime) / 1000);
 
         var now = new Date();
